2d3c831143f0cbbda6ca55c39289ca534a5e3b30
JaeWoong An <jaewoongan2@gmail.com>
2023-06-01 07:06:03 +0900

    commit 1 - SecondBranch

--- app/src/main/java/com/example/gittest/MainActivity.kt
+++ app/src/main/java/com/example/gittest/MainActivity.kt
@@ -7,6 +7,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        println("git check 3")
+        println("git check 1 - Second Branch")
     }
 }
