fec83d4c76232c55b39ada16424a5a937bc305f2
JaeWoong An <jaewoongan2@gmail.com>
2023-06-01 09:11:33 +0900

    commit ex 1

---
+++ app/src/main/java/com/example/gittest/ExActivity.kt
@@ -0,0 +1,11 @@
+package com.example.gittest
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ExActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        println("1")
+    }
+}
